b68bf67e307172a753caa8963dd75716fc80ac49
nzomok <caesarjjoshua@gmail.com>
2023-06-22 01:28:53 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,10 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c >= '0'; c != '2' && c != '4'; c <= '9'; c++)
+	for (c >= '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	}
 	{
 		_putchar (c);
 	}
